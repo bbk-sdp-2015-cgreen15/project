2321756c20a8842d7d0a3ce1135854d71397ccb6
bbk-sdp-2015-cgreen15 <craig.sg@gmail.com>
2015-09-05 16:08:17 +0100

    Refactor part 2 - more moving stuff around

--- public/js/fe20.js
+++ public/js/fe20.js
@@ -5,25 +5,14 @@
 console.log('fe20');
 var ONE_MINUTE = 60 * 1000;
 
+
+
 (function () {
 
     'use strict';
 
     //*******************************************************************************
     // Constants + Pseudo Globals
-
-    var DATE_FORMAT = "d MM yy";
-    var WIDGET_WIDTH = 600;
-    var WIDGET_HEIGHT = 400;
-    var SHOWSPEED = 800;
-
-    var STAGE_DETAILS = 1;
-    var STAGE_TABLE = 2;
-    var STAGE_CHART = 3;
-
-    var RESIZABLE = false;
-    var widgetList = {};    // Hash List !
-    var widgetHelpers;
 
     var templateIdList = [
         'widget-template',
@@ -44,74 +33,8 @@ var ONE_MINUTE = 60 * 1000;
     // Orchestration functions
 
 
-    function updateBalances(wid, sparseTimeSeries) {
-
-        var widget = widgetList[wid];
-        var tableEntries = widget.ko.tableEntries();
-        var i = 0;
-
-        $.each(sparseTimeSeries, function (key, value) {
-
-            // update the display balance
-            tableEntries[i].balance = value.balance;
-            i++;
-        });
-
-        widget.ko.tableEntries(null);
-        widget.ko.tableEntries(tableEntries);
-    }
 
 
-    function makeChartSeries(wid, callback) {
-
-        var widget = widgetList[wid];
-        var attributes = widget.attributes;
-        var tableEntries = widget.ko.tableEntries();
-
-        // Get chartData from the service
-        $.ajax({
-            url: "/app/tst",
-            type: "post",
-            data: JSON.stringify({
-                attributes: attributes,
-                tableEntries: tableEntries
-            }),
-            contentType: "application/json",
-            dataType:'json'
-
-        }).success(function (data) {
-
-            widget.chartData = data.chartData;
-            var sparseTimeSeries = data.sparseTimeSeries;
-            console.log('sparseTimeSeries');
-            console.log(sparseTimeSeries);
-            updateBalances(wid, sparseTimeSeries);
-            callback();
-        });
-    }
-
-
-    function goToTablyWithDates(wid) {
-
-        var widget = widgetList[wid];
-        var attributes = widget.attributes;
-
-        // Get TableData from the service
-        $.ajax({
-            url: "/app/tabledata",
-            type: "post",
-            data: attributes
-        }).success(function(tableData) {
-
-            widget.ko.tableEntries(tableData);
-            widget.editStage2Value = true;
-
-            makeChartSeries(wid, function () {
-
-                widgetHelpers.showCharty(wid);
-            });
-        });
-    }
 
     //*******************************************************************************
     // MAIN
--- public/js/utils/utils.js
+++ public/js/utils/utils.js
@@ -29,3 +29,105 @@ function compileTemplates(templateIdList, templateCompiledList) {
         templateCompiledList[templateId] = Handlebars.compile(template);
     }
 }
+
+
+
+
+
+
+//*****************************************************************************************
+//*****************************************************************************************
+// UI Functions
+
+
+function goToTablyWithDates(wid) {
+
+    var widget = widgetList[wid];
+    var attributes = widget.attributes;
+
+    // Get TableData from the service
+    $.ajax({
+        url: "/app/tabledata",
+        type: "post",
+        data: attributes
+    }).success(function(tableData) {
+
+        widget.ko.tableEntries(tableData);
+        widget.editStage2Value = true;
+
+        makeChartSeries(wid, function () {
+
+            widgetHelpers.showCharty(wid);
+        });
+    });
+}
+
+
+function makeChartSeries(wid, callback) {
+
+    var widget = widgetList[wid];
+    var attributes = widget.attributes;
+    var tableEntries = widget.ko.tableEntries();
+
+    // Get chartData from the service
+    $.ajax({
+        url: "/app/tst",
+        type: "post",
+        data: JSON.stringify({
+            attributes: attributes,
+            tableEntries: tableEntries
+        }),
+        contentType: "application/json",
+        dataType:'json'
+
+    }).success(function (data) {
+
+        widget.chartData = data.chartData;
+        var sparseTimeSeries = data.sparseTimeSeries;
+        console.log('sparseTimeSeries');
+        console.log(sparseTimeSeries);
+        updateBalances(wid, sparseTimeSeries);
+        callback();
+    });
+}
+
+
+function updateBalances(wid, sparseTimeSeries) {
+
+    var widget = widgetList[wid];
+    var tableEntries = widget.ko.tableEntries();
+    var i = 0;
+
+    $.each(sparseTimeSeries, function (key, value) {
+
+        // update the display balance
+        tableEntries[i].balance = value.balance;
+        i++;
+    });
+
+    widget.ko.tableEntries(null);
+    widget.ko.tableEntries(tableEntries);
+}
+
+
+
+
+
+//*****************************************************************************************
+//*****************************************************************************************
+// Pseudo Globals
+
+var widgetList = {};    // Hash List !
+
+var DATE_FORMAT = "d MM yy";
+var WIDGET_WIDTH = 600;
+var WIDGET_HEIGHT = 400;
+var SHOWSPEED = 800;
+
+var STAGE_DETAILS = 1;
+var STAGE_TABLE = 2;
+var STAGE_CHART = 3;
+
+var RESIZABLE = false;
+
+var widgetHelpers;
